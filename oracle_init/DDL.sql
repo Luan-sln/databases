2e22a3d7f8a2734867c5f78841a40ac16a999ece
luanpedrangelo <luanpedrangello@hotmail.com>
2024-04-03 21:02:45 -0300

    add new role

--- oracle_init/DDL.sql
+++ oracle_init/DDL.sql
@@ -2,3 +2,6 @@ alter session set "_ORACLE_SCRIPT"=true;
 
 CREATE USER engenharia IDENTIFIED BY engenharia;
 GRANT ALL PRIVILEGES TO engenharia;
+
+CREATE USER pessoal IDENTIFIED BY pessoal;
+GRANT ALL PRIVILEGES TO pessoal;
